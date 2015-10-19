773d532236c27702d89bc8b1c6f6305a7428d4fe
Lee Brunjes <lee.brunjes@gmail.com>
2015-10-19 13:36:03 -0400

    Added serial monitor that reads teh serial port and makes request based on ints coming in.

---
+++ SerialMonitor/Config.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SerialMonitor
+{
+	public abstract class Config
+	{
+
+		protected Dictionary<string,string> ConfigFile= new Dictionary<string, string>();
+		protected Dictionary<string,string> CommandArgs = new Dictionary<string, string>();
+
+		public Config (string file=null, string[] cmdlineArgs = null)
+		{
+
+			//readCommand config filke
+			if (file != null) {
+				ConfigFile = readConfig (file);
+			}
+			//read args
+			if (cmdlineArgs != null) {
+				CommandArgs = readArgs (cmdlineArgs);
+			}
+			//asign values
+			AssignValues ();
+
+		}
+		protected virtual void AssignValues(){
+			FieldInfo[] fields = this.GetType ().GetFields();
+			for (var i = 0; i < fields.Length; i++) {
+				if (ConfigFile.ContainsKey (fields [i].Name)) {
+
+				}
+				if (CommandArgs.ContainsKey (fields [i].Name)) {
+
+				}
+
+			}
+		}
+
+		protected void AssignField(FieldInfo field, string value){
+			try{
+				field.SetValue (this, value);
+			}
+			catch(Exception ex){
+				Console.WriteLine("failed to set value for "+field.Name+
+					" ("+value+")\n"+ex.Message);
+			}
+		}
+
+		//read the config file into a dictopary
+		protected  Dictionary<string,string> readConfig (string filename)
+		{
+			Dictionary<string,string> dic = new Dictionary<string, string> ();
+			try {
+				var lines = File.ReadLines (filename);
+				foreach (string line in lines) {
+					string tmp = line;
+					if (line.IndexOf ("#") >= 0) {
+						tmp = line.Substring (0, line.IndexOf ("#"));
+					}
+					if (tmp.Length > 3) {
+						var strings = tmp.Split ('=');
+						if (strings.Length == 2) {
+							dic.Add (strings [0].Trim (), strings [1].Trim ());
+						}
+					}
+
+				}
+			} catch (Exception ex) {
+				Console.WriteLine ("ERROR: cannot read config file.");
+				Console.WriteLine ("Reason:" + ex.Message);
+				Console.WriteLine ("trace:" + ex.StackTrace);
+			}
+
+			return dic;
+
+		}
+
+		protected  Dictionary<string,string> readArgs (string[] args)
+		{
+			Dictionary<string,string> dic = new Dictionary<string, string> ();
+				
+			string key;
+			string command_line_flags = "";
+			FieldInfo[] fields = this.GetType ().GetFields();
+			for (int i = 0; i < args.Length; i++) {
+
+				key = args [1].TrimStart('-');
+				bool oneOfUs = false;
+				for (int j = 0; j < fields.Length; j++) {
+					if (fields [j].Name == key) {
+						oneOfUs = true;
+						break;
+					}
+				}
+
+				if(oneOfUs && i+1 < args.Length){
+					dic.Add(key, args[i++]);
+				}
+				else{
+					command_line_flags+= args[i];
+				}
+
+
+			}
+
+			dic.Add ("command_line_flags", command_line_flags);
+
+			return dic;
+		}
+
+		public virtual new string ToString(){
+			StringBuilder sb = new StringBuilder ();
+			sb.Append ("Config File:\n");
+			foreach(string key in ConfigFile.Keys){
+				sb.AppendFormat ("   {0}={1}\n", key, ConfigFile [key]);
+			}
+			sb.Append ("Command Line:\n");
+			foreach(string key in CommandArgs.Keys){
+				sb.AppendFormat ("   {0}={1}\n", key, CommandArgs [key]);
+			}
+			return sb.ToString ();
+		}
+
+	}
+}
+
---
+++ SerialMonitor/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO.Ports;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+
+namespace SerialMonitor
+{
+	class SerialMonitorConfig:Config{
+		public string portname = "/dev/ttyACM0";
+		public int baudrate = 9600;
+		public int readtimeout = 10000;
+		public int writetimeout = 500;
+		public string url = "http://127.0.0.1:8080/Service.ashx";
+		public string username = "admin";
+		public string password = "password";
+		public Dictionary<int, string> networkCommands = new Dictionary<int, string>();
+		public TimeSpan DisableTimer = new TimeSpan (0, 0, 5);
+
+		public SerialMonitorConfig(string file, string[] args):base (file, args){
+
+		}
+
+		protected override void AssignValues ()
+		{
+			base.AssignValues ();
+
+			//deal with the network commands...
+			String join = "?type=babyevents&eventtype={0}&subtype={1}&details={2}";
+			networkCommands.Add (2, String.Format (join,
+				ConfigFile ["networkCommands:2"].Split (',')));
+			networkCommands.Add (3, String.Format (join,
+				ConfigFile ["networkCommands:3"].Split (',')));
+			networkCommands.Add (4, String.Format (join,
+				ConfigFile ["networkCommands:4"].Split (',')));
+			networkCommands.Add (5, String.Format (join,
+				ConfigFile ["networkCommands:5"].Split (',')));
+		}
+
+	}
+
+	class MainClass
+	{
+		public static SerialMonitorConfig activeConfig;
+		protected static Dictionary<int, DateTime> timeouts =new Dictionary<int, DateTime>();
+
+		public static void Main (string[] args)
+		{
+			activeConfig = new SerialMonitorConfig ("Serial.conf",args);
+
+			//assign time outs to all network commands
+			foreach (int  i in activeConfig.networkCommands.Keys) {
+				timeouts.Add (i, DateTime.Now);
+			}
+
+			stamp ();
+		
+			SerialPort serialPort = new SerialPort ();
+			serialPort.BaudRate = activeConfig.baudrate;
+			serialPort.PortName = activeConfig.portname;
+		//serialPort.Parity = activeConfig.Parity;
+			serialPort.ReadTimeout = activeConfig.readtimeout;
+			serialPort.WriteTimeout = activeConfig.writetimeout;
+
+			serialPort.Open ();
+			string message;
+			while (true) {
+				try{
+					message= serialPort.ReadLine ();
+
+					int cmd = 0;
+					if(int.TryParse(message, out cmd)){
+						readCommand(cmd);
+					}
+
+
+				
+				}
+				catch(Exception ex){
+					//
+				}
+			}
+		}
+		public static void stamp(){
+			Console.WriteLine (@"
+SERIAL MONITOR
+For use with BabyData
+CONFIG:
+");
+			Console.WriteLine (activeConfig.ToString ());
+		}
+
+
+		public static void readCommand (int value){
+			//read in teh command.
+			if (value > 1 && value < 6) {
+				//we pushed teh button
+				if (DateTime.Now > timeouts [value]) {
+					timeouts [value] = DateTime.Now.AddSeconds (5);
+					MakeRequest (activeConfig.url + activeConfig.networkCommands [value]);
+
+				}
+			} else if (value == 6) {
+				Console.WriteLine ("toggle sleep state");
+				if (activeConfig.networkCommands [5].Contains ("up")) {
+					activeConfig.networkCommands [5] = activeConfig.networkCommands [5].Replace ("up", "down");
+				} else {
+					activeConfig.networkCommands [5] = activeConfig.networkCommands [5].Replace ("down", "up");
+				}
+			} else {
+				Console.WriteLine ("invalid option Expected [2-6] got:" + value);
+			}
+		}
+
+
+		public static void MakeRequest(string url){
+			Console.WriteLine("REQUEST: "+url);
+			HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create (url);
+			req.Method = "POST";
+			req.Headers ["Authorization"] = "Basic " +
+			Convert.ToBase64String (Encoding.Default.GetBytes (
+					activeConfig.username+":"+activeConfig.password
+			));
+
+			StreamReader r = new StreamReader(req.GetResponse ().GetResponseStream());
+
+			Console.WriteLine(r.ReadToEnd());
+			
+
+		}
+
+	}
+}
